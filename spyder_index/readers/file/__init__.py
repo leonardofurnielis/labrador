e98cdc5e41f8b325976d8173eeceef24f874f16b
Leonardo Furnielis <leonardofurnielis@outlook.com>
2024-05-13 17:34:44 -0300

    add docx reader

--- spyder_index/readers/file/__init__.py
+++ spyder_index/readers/file/__init__.py
@@ -1,7 +1,9 @@
+from spyder_index.readers.file.docx import DocxReader
 from spyder_index.readers.file.json import JSONReader
 from spyder_index.readers.file.pdf import PDFReader
 
 __all__ = [
+    "DocxReader",
     "JSONReader",
     "PDFReader",
 ]
