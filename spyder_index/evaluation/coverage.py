cc8e10e47effcff0edbc067a810ee83fa9ec1e7c
Leonardo Furnielis <leonardofurnielis@outlook.com>
2024-07-13 08:05:26 -0300

    evaluation code typing

--- spyder_index/evaluation/coverage.py
+++ spyder_index/evaluation/coverage.py
@@ -5,6 +5,7 @@ from spyder_index.core.embeddings import BaseEmbedding
 
 from pydantic.v1 import BaseModel
 
+
 class KnowledgeBaseCoverage(BaseModel):
     """Indicates how much the KnowledgeBase has contributed to the answer's coverage."""
 
@@ -16,20 +17,19 @@ class KnowledgeBaseCoverage(BaseModel):
         arbitrary_types_allowed = True
 
     def evaluate(self, contexts: List[str], output: str):
-                            
+
         if not contexts or not output:
             raise ValueError("Must provide these parameters [`contexts`, `output`]")
-            
-        coverage = { "contexts_score": [], "score": 0 }
+
+        coverage = {"contexts_score": [], "score": 0}
         output_embedding = self.embed_model.get_query_embedding(output)
-            
+
         for context in contexts:
             context_embedding = self.embed_model.get_query_embedding(context)
-            coverage["contexts_score"].append(self.embed_model.similarity(output_embedding, context_embedding, mode=self.similarity_mode))
+            coverage["contexts_score"].append(
+                self.embed_model.similarity(output_embedding, context_embedding, mode=self.similarity_mode))
 
         coverage["score"] = np.mean(coverage["contexts_score"])
         coverage["passing"] = coverage["score"] >= self.similarity_threshold
 
         return coverage
-            
-
