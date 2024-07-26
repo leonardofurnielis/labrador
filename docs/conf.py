3572692346da1a2d4b126ec6dd6df377060ee62a
Leonardo Furnielis <leonardofurnielis@outlook.com>
2024-07-26 17:54:54 -0300

    update conf.py sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,6 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
-from pathlib import Path
 
 project = 'spyder-index'
 copyright = '2024, Leonardo Furnielis'
@@ -17,13 +16,8 @@ release = '0.2.7'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-"""Setup project path for sphinx and verify that it's valid"""
-for idx, dir in enumerate(["../"]):
-    src_dir = Path(os.path.abspath(dir))
-    if not src_dir.exists():
-        raise Exception(f"Provided project directory '{src_dir}' does not exists!")
-
-    sys.path.insert(idx, str(src_dir.absolute()))
+"""Setup project path for sphinx"""
+sys.path.insert(0, os.path.abspath("../"))
 
 extensions = [
     "sphinx_copybutton",
@@ -48,5 +42,6 @@ autodoc_default_options = {"exclude-members": "__init__"}
 html_theme = 'furo'
 html_static_path = ['_static']
 html_title = "🕸️ spyder-index Framework"
+html_last_updated_fmt = "%b %d, %Y"
 html_copy_source = False
 html_show_sourcelink = False
