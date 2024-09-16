0fa1779afb1f98c7e90ca8dde92689f48e735d32
Leonardo Furnielis <leonardofurnielis@outlook.com>
2024-09-16 11:38:34 -0300

    docs: fix example

--- spyder_index/embeddings/watsonx.py
+++ spyder_index/embeddings/watsonx.py
@@ -65,9 +65,7 @@ class WatsonxEmbedding(BaseModel, BaseEmbedding):
         A way you may use:
         >>> from spyder_index.embeddings import WatsonxEmbedding
         >>> text = "A python data library for building AI applications"
-        >>> watsonx_embed = WatsonxEmbedding(api_key="<you_api_key>",
-                                             url="https://us-south.ml.cloud.ibm.com",
-                                             project_id="<your_project_id>")
+        >>> watsonx_embed = WatsonxEmbedding(api_key="<you_api_key>", url="https://us-south.ml.cloud.ibm.com", project_id="<your_project_id>")
         """
         embedding_text = self.get_texts_embedding([query])[0]
 
