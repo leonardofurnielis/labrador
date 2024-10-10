aa9b9ca355856df5de5319fd761476c85d6f3cfe
Leonardo Furnielis <leonardofurnielis@outlook.com>
2024-10-10 15:14:36 -0300

    remove langchain pypdf loader

--- deeptxt/readers/file/pdf.py
+++ deeptxt/readers/file/pdf.py
@@ -6,8 +6,6 @@ from typing import List, Optional
 from deeptxt.core.readers import BaseReader
 from deeptxt.core.document import Document
 
-from langchain_community.document_loaders.pdf import PyPDFLoader
-
 
 class PDFReader(BaseReader):
     """PDF reader using PyPDF.
@@ -29,10 +27,23 @@ class PDFReader(BaseReader):
         if not os.path.isfile(input_file):
             raise ValueError(f"File `{input_file}` does not exist")
 
-        self.input_file = Path(input_file)
+        self.input_file = str(Path(input_file).resolve())
 
     def load_data(self, extra_info: Optional[dict] = None) -> List[Document]:
         """Loads data from the specified directory."""
-        lc_documents = PyPDFLoader(file_path=self.input_file).load()
+        try:
+            import pypdf  # noqa: F401
+        except ImportError:
+            raise ImportError("pypdf package not found, please install it with `pip install pypdf`")
 
-        return [Document().from_langchain_format(doc=doc) for doc in lc_documents]
+        pdf_reader = pypdf.PdfReader(self.input_file)
+
+        documents = [
+            Document(
+                text=page.extract_text().strip(),
+                metadata={"source": self.input_file, "page": page_number}
+            )
+            for page_number, page in enumerate(pdf_reader.pages)
+        ]
+
+        return documents
