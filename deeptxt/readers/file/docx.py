a5eac7925779e0156dc7054749a64e766ae0143d
Leonardo Furnielis <leonardofurnielis@outlook.com>
2024-10-11 16:35:41 -0300

    remove langchain docx reader

--- deeptxt/readers/file/docx.py
+++ deeptxt/readers/file/docx.py
@@ -6,8 +6,6 @@ from typing import List, Optional
 from deeptxt.core.readers import BaseReader
 from deeptxt.core.document import Document
 
-from langchain_community.document_loaders import Docx2txtLoader
-
 
 class DocxReader(BaseReader):
     """Microsoft Word (Docx) reader.
@@ -17,22 +15,22 @@ class DocxReader(BaseReader):
     """
 
     def __init__(self, input_file: str = None):
-
-        try:
-            import docx2txt  # noqa: F401
-        except ImportError:
-            raise ImportError("docx2txt package not found, please install it with `pip install docx2txt`")
-
         if not input_file:
             raise ValueError("You must provide a `input_dir` parameter")
 
         if not os.path.isfile(input_file):
             raise ValueError(f"File `{input_file}` does not exist")
 
-        self.input_file = Path(input_file)
+        self.input_file = str(Path(input_file).resolve())
 
     def load_data(self, extra_info: Optional[dict] = None) -> List[Document]:
         """Loads data from the specified directory."""
-        lc_documents = Docx2txtLoader(file_path=self.input_file).load()
+        try:
+            import docx2txt  # noqa: F401
+        except ImportError:
+            raise ImportError("docx2txt package not found, please install it with `pip install docx2txt`")
 
-        return [Document().from_langchain_format(doc=doc) for doc in lc_documents]
+        text = docx2txt.process(self.input_file)
+        metadata = {"source": self.input_file}
+
+        return [Document(text=text, metadata=metadata)]
