03eacb85d015d2b93d70223ee4570053cc9fd2bf
Leonardo Furnielis <leonardofurnielis@outlook.com>
2024-12-11 18:11:03 -0300

    v0.4.5

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ import sys
 project = 'tempestai'
 copyright = '2024, Leonardo Furnielis'
 author = 'Leonardo Furnielis'
-version = '0.4.4'
+version = '0.4.5'
 release = version
 
 # -- General configuration ---------------------------------------------------
