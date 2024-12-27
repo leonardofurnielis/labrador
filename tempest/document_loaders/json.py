826bd0350f4377e8ec82a07ea6159bcbd643a459
Leonardo Furnielis <leonardofurnielis@outlook.com>
2024-12-27 15:26:53 -0300

    remove lc from json loader

--- tempest/document_loaders/json.py
+++ tempest/document_loaders/json.py
@@ -1,8 +1,7 @@
 import os
+import json
 from pathlib import Path
 from typing import List, Optional
-
-from langchain_community.document_loaders import JSONLoader as LangChainJSONLoader
 
 from tempest.core.document import Document
 from tempest.core.document_loaders import BaseLoader
@@ -13,11 +12,9 @@ class JSONLoader(BaseLoader):
 
     Args:
         jq_schema (str, optional): jq schema to use to extract the data from the JSON.
-        text_content (bool, optional): Flag to indicate whether the content is in string format. Default is ``False``
     """
 
     jq_schema: Optional[str] = None
-    text_content: Optional[bool] = False
 
     def load_data(self, input_file: str, extra_info: Optional[dict] = None) -> List[Document]:
         """Loads data from the specified directory.
@@ -33,10 +30,24 @@ class JSONLoader(BaseLoader):
         if not os.path.isfile(input_file):
             raise ValueError(f"File `{input_file}` does not exist")
         
-        input_file = str(Path(input_file).resolve())
+        documents = []
+        jq_compiler = jq.compile(self.jq_schema)
+        json_file = Path(input_file).resolve().read_text(encoding="utf-8")
+        json_data = jq_compiler.input(json.loads(json_file))
         
-        lc_documents = LangChainJSONLoader(file_path=input_file,
-                                  jq_schema=self.jq_schema,
-                                  text_content=self.text_content).load()
+        
+        for content in json_data:
 
-        return [Document().from_langchain_format(doc=doc) for doc in lc_documents]
+            if isinstance(content, str):
+                content = content
+            elif isinstance(content, dict):
+                content = json.dumps(content) if content else ""
+            else:
+                content = str(content) if content is not None else ""
+            
+            if content.strip() != "":
+                documents.append(Document(
+                    text=content,
+                    metadata={"source": str(Path(input_file).resolve())}))
+
+        return documents
