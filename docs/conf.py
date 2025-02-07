481f4dce671dc05f408ed6dc360d40fb3c54a4b7
Leonardo Furnielis <leonardofurnielis@outlook.com>
2025-02-07 17:20:53 -0300

    v0.6.1

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ import sys
 project = 'Labrador'
 copyright = '2024, Leonardo Furnielis'
 author = 'Leonardo Furnielis'
-version = '0.6.0'
+version = '0.6.1'
 release = version
 
 # -- General configuration ---------------------------------------------------
