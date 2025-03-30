39488744f3d02d7873ab14c34e25a69b51a5b625
Leonardo Furnielis <leonardofurnielis@outlook.com>
2025-03-30 19:34:12 -0300

    fix: wxgovernance cloud issue wml and facts creds

--- beekeeper/monitor/watsonx.py
+++ beekeeper/monitor/watsonx.py
@@ -185,7 +185,7 @@ class WatsonxExternalPromptMonitoring:
         )
             
         try:
-            if self._fact_cpd_creds: 
+            if hasattr(self, '_fact_cpd_creds') and self._fact_cpd_creds:
                 cpd_creds = CloudPakforDataConfig(**self._fact_cpd_creds)
                 
                 aigov_client = AIGovFactsClient(
@@ -220,7 +220,7 @@ class WatsonxExternalPromptMonitoring:
         from ibm_watsonx_ai import APIClient, Credentials  # type: ignore
             
         try:
-            if self._wml_cpd_creds:
+            if hasattr(self, '_wml_cpd_creds') and self._wml_cpd_creds:
                 creds = Credentials(**self._wml_cpd_creds)
                 
                 wml_client = APIClient(creds)
@@ -563,7 +563,7 @@ class WatsonxPromptMonitoring:
         )
             
         try:
-            if self._fact_cpd_creds: 
+            if hasattr(self, '_fact_cpd_creds') and self._fact_cpd_creds:
                 cpd_creds = CloudPakforDataConfig(**self._fact_cpd_creds)
                 
                 aigov_client = AIGovFactsClient(
@@ -598,7 +598,7 @@ class WatsonxPromptMonitoring:
         from ibm_watsonx_ai import APIClient, Credentials  # type: ignore
             
         try:
-            if self._wml_cpd_creds:
+            if hasattr(self, '_wml_cpd_creds') and self._wml_cpd_creds:
                 creds = Credentials(**self._wml_cpd_creds)
                 
                 wml_client = APIClient(creds)
