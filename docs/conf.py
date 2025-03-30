bfcc2b16947900aff094c53f9a1eee40c208bd02
Leonardo Furnielis <leonardofurnielis@outlook.com>
2025-03-30 20:20:19 -0300

    v0.6.6

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ import sys
 project = 'Beekeeper'
 copyright = '2024, Leonardo Furnielis'
 author = 'Leonardo Furnielis'
-version = '0.6.5'
+version = '0.6.6'
 release = version
 
 # -- General configuration ---------------------------------------------------
