c1d4e936f3534a8bdfb58baafb7c9d85b2fcd28d
Leonardo Furnielis <leonardofurnielis@outlook.com>
2025-04-06 23:01:51 -0300

    feat: new ingistion flow

--- pineflow/core/flows/ingestion_flow.py
+++ pineflow/core/flows/ingestion_flow.py
@@ -6,14 +6,15 @@ from pineflow.core.readers.base import BaseReader
 
 
 class IngestionFlow():
-
     def __init__(self, 
+                 transformers,
                  readers: List[BaseReader]=None):
         
+        self.transformers = transformers
         self.readers = readers
     
     def _read_documents(self, documents: Optional[List[Document]]):
-        input_documents = List[Document] = []
+        input_documents = []
         
         if documents is not None:
             input_documents.extend(documents)
@@ -24,16 +25,16 @@ class IngestionFlow():
         
         return input_documents    
         
-    def _transform(documents: List[Document], transformations):
-        for transform in transformations:
-            documents = transform(documents)
+    def _run_transformers(self, documents: List[Document], transformers):
+        for transformer in transformers:
+            documents = transformer(documents)
         
         return documents    
     
     def run(self, documents: List[Document]=None):
         input_documents = self._read_documents(documents)
         
-        documents = self._transform(input_documents)
+        documents = self._run_transformers(input_documents, self.transformers)
         
         return documents or []
         
