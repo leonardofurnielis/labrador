4da2219422b50c49037251e47df20b17cf55915d
Leonardo Furnielis <leonardofurnielis@outlook.com>
2025-04-17 11:50:27 -0300

    v0.6.5

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ import sys
 project = 'Pineflow'
 copyright = '2025, Leonardo Furnielis'
 author = 'Leonardo Furnielis'
-version = '0.6.4'
+version = '0.6.5'
 release = version
 
 # -- General configuration ---------------------------------------------------
